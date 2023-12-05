b0e47fbe6b51654aa6adaa95880d0aa143d750e5
azadaliyev <azadaliyev44@gmail.com>
2023-12-05 15:19:42 +0100

    revevrt trial

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from tasks.task_list import TaskList
 from tasks.subtask import Subtask
 import datetime
 
-# I use here to try out the functions yet
+archive = TaskList().archive
 task_list = TaskList()
 
 
