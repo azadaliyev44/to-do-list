3a1c6afb61ab578778ff4aaaf04a727ce7958732
azadaliyev44 <68191048+azadaliyev44@users.noreply.github.com>
2024-01-23 20:55:36 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,17 @@
-print(' Hər kəsə salam \n 33 yaşım var ')
+class ImmutableList:
+    def __init__(self, items):
+        self.items = tuple(items)
+
+    def add_item(self, new_item):
+        return ImmutableList(self.items + (new_item,))
+
+    def get_items(self):
+        return self.items
+
+def transform_list(immutable_list: ImmutableList, transformer_function) -> ImmutableList:
+    return ImmutableList(map(transformer_function, immutable_list.get_items()))
+
+original_list = ImmutableList([1, 2, 3])
+squared_list = transform_list(original_list, lambda x: x**2)
+print("Original List:", original_list.get_items())
+print("Squared List:", squared_list.get_items())
